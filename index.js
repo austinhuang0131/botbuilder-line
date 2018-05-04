6a33f1a691ffcd79e34d6c047d96bdf27c2f4f59
Austin Huang <austinhuang0131@gmail.com>
2018-05-04 15:17:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function Create(options) {
         "BotBuilder-Line > No more than 5 messages to 1 reply token! Messages: " +
           JSON.stringify(messages)
       );
-    else if (messages.filter(m => m.address.source === "line").length !== messages.length)
+    else if (messages.filter(m => m.source === "line").length !== messages.length)
       return Promise.reject(
         "BotBuilder-Line > Ignoring messages for other platforms..." +
           JSON.stringify(messages)
