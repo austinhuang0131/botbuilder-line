46e070a2294994e645c8d41568fb87780463d89f
Austin Huang <austinhuang0131@gmail.com>
2018-05-21 13:21:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -57,158 +57,159 @@ getAltText = (s) => {return s.substring(0, 400);};
 // options:
 // channelAccessToken / channelSecret: why explain
 // debug: bool, show a bunch of useless console logs
-
 function LineConnector(options) {
-  options = Object.assign({ channelId: channelId }, options);
-  if (!options.channelAccessToken || !options.channelSecret)
-    throw 'BotBuilder-Line > Options undefined! Define them as the following: {channelAccessToken: "token", channelSecret: "secret"}';
-  var line = new Line.Client(options);
-  this.onEvent = (handler) => this.handler = handler;
-  this.startConversation = () => {if (options.debug) console.log("BotBuilder-Line > startConversation", arguments)};
-  this.onInvoke = () => {if (options.debug) console.log("BotBuilder-Line > onInvoke", arguments)};
-  this.processLineMessage = function(message) {
-    if (message.type === "message") {
-      var msg = {
-          timestamp: message.timestamp,
-          source: "line",
-          replyToken: message.replyToken,
-          entities: [],
-          address: {
-              bot: { name: "placeholder", id: "placeholder" },
-              user: { name: message.replyToken, id: message.source.userId },
-              channelId: "line",
-              channelName: "line",
-              msg: message,
-              conversation: {id: message.source.groupId ? message.source.groupId : "1", isGroup: message.source.type === "group" ? true : false}
-          },
-      };
-      if (message.message.type === "text") msg.text = message.message.text;
-      this.handler([msg]);
-      if (options.debug) console.log("BotBuilder-Line > Message processed", msg);
-    }
-    return this;
-  };
-  this.listen = function(req, res) {
-    res.send("ok");
-    const signature = crypto.createHmac("SHA256", options.channelSecret)
-      .update(JSON.stringify(req.body))
-      .digest("base64");
-    if (options.debug) console.log("BotBuilder-Line > Messages received", req.body);
-    if (signature !== req.get("X-Line-Signature")) {
-      return console.error(
-        "BotBuilder-Line > Request trashed due to signature mismatch. Body: ", req.body
-      );
-    } else {
-      return Promise.all(req.body.events.map(this.processLineMessage));
-    }
-  };
-  Object.assign(line, options);
-  return this;
-}
-
-LineConnector.prototype.send = function(messages, cb) {
-  if (messages.length > 5)
-    return Promise.reject(
-      "BotBuilder-Line > No more than 5 messages to 1 reply token! Messages: " +
-        JSON.stringify(messages)
-    );
-  else if (messages.filter(m => m.source === "line").length !== messages.length)
-    return Promise.reject(
-      "BotBuilder-Line > Ignoring messages for other platforms..." +
-        JSON.stringify(messages)
-    );
-  if (options.debug)
-    console.log(
-      "BotBuilder-Line > Sending messages... " + JSON.stringify(messages)
-    );
-    var body = [];
-    messages.map(msg => {
-      if (msg.attachments && msg.attachments.map(t => t.contentType).filter((value, index, self) => {return self.indexOf(value) === index;}).length > 1) throw "BotBuilder-Line > All attachments in one message must have the same ContentType."
-      else if (msg.attachments && msg.attachments.length > 1 && msg.attachments.length < 11 && msg.attachmentLayout === "carousel" && msg.attachments[0].contentType === "application/vnd.microsoft.card.hero") {
-        body.push({
-          type: "template",
-          altText: getAltText(msg.text || msg.title + " " + msg.subtitle || "Please select an action."),
-          template: {
-            type: "carousel",                        
-            columns: msg.attachments.map(a => {
-              var m = {
-                text: a.content.text,
-                title: a.content.title || null,
-                actions: a.content.buttons.map(b => getButtonTemp(b))
-              }
-              if (a.images) m.thumbnailImageUrl = m.content.images[0].url;
-              return m;
-            })
-          }
-        });
+  function lineConnector(options) {
+    options = Object.assign({ channelId: channelId }, options);
+    if (!options.channelAccessToken || !options.channelSecret)
+      throw 'BotBuilder-Line > Options undefined! Define them as the following: {channelAccessToken: "token", channelSecret: "secret"}';
+    var line = new Line.Client(options);
+    this.onEvent = (handler) => this.handler = handler;
+    this.startConversation = () => {if (options.debug) console.log("BotBuilder-Line > startConversation", arguments)};
+    this.onInvoke = () => {if (options.debug) console.log("BotBuilder-Line > onInvoke", arguments)};
+    this.processLineMessage = function(message) {
+      if (message.type === "message") {
+        var msg = {
+            timestamp: message.timestamp,
+            source: "line",
+            replyToken: message.replyToken,
+            entities: [],
+            address: {
+                bot: { name: "placeholder", id: "placeholder" },
+                user: { name: message.replyToken, id: message.source.userId },
+                channelId: "line",
+                channelName: "line",
+                msg: message,
+                conversation: {id: message.source.groupId ? message.source.groupId : "1", isGroup: message.source.type === "group" ? true : false}
+            },
+        };
+        if (message.message.type === "text") msg.text = message.message.text;
+        this.handler([msg]);
+        if (options.debug) console.log("BotBuilder-Line > Message processed", msg);
       }
-      else if (msg.attachments) {
-        return msg.attachments.map(a => {
-          switch (a.contentType.split("/")[0]) {
-            case "image":
-              body.push({
-                type: "image",
-                originalContentUrl: a.contentUrl,
-                previewImageUrl: a.thumbnailUrl || a.contentUrl
-              });
-              break;
-            case "video":
-              body.push({
-                type: "video",
-                originalContentUrl: a.contentUrl,
-                previewImageUrl:
-                  a.thumbnailUrl ||
-                  "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/video-128.png"
-                // Either you define a thumbnailUrl or I use a stock image
-              });
-              break;
-            case "audio":
-              body.push({
-                type: "audio",
-                originalContentUrl: a.contentUrl,
-                duration: getDuration(a.contentUrl)
-              });
-              break;
-            case "application":
-              switch (a.contentType.split("/")[1]) {
-                case "vnd.microsoft.keyboard":
-                  if (a.content.buttons.length < 5) body.push({
-                    type: "template",
-                    altText: getAltText(msg.text || msg.title + " " + msg.subtitle || "Please select an action."),
-                    template: {
-                      type: "buttons",
-                      title: msg.title || null,
-                      text: msg.text || "Please select an action.",
-                      actions: a.content.buttons.map(b => getButtonTemp(b))
-                    }
-                  })
-                  else throw "BotBuilder-Line > If you have more than 4 buttons, use a carousel of hero cards instead (Up to 10 cards with 3 buttons each)."
-                  break;
-                case "vnd.microsoft.card.hero":
-                  if (a.content.buttons.length < 5) body.push({
-                    type: "template",
-                    altText: getAltText(a.content.text || a.content.title + " " + a.content.subtitle || "Please select an action."),
-                    template: {
-                      type: "buttons",
-                      title: a.content.title || null,
-                      text: a.content.text || "Please select an action.",
-                      actions: a.content.buttons.map(b => getButtonTemp(b))
-                    }
-                  })
-                  else throw "BotBuilder-Line > If you have more than 4 buttons, use a carousel of hero cards instead (Up to 10 cards with 3 buttons each)."
-                  break;
-              }
-              break;
-          }
-        });
+      return this;
+    };
+    this.listen = function(req, res) {
+      res.send("ok");
+      const signature = crypto.createHmac("SHA256", options.channelSecret)
+        .update(JSON.stringify(req.body))
+        .digest("base64");
+      if (options.debug) console.log("BotBuilder-Line > Messages received", req.body);
+      if (signature !== req.get("X-Line-Signature")) {
+        return console.error(
+          "BotBuilder-Line > Request trashed due to signature mismatch. Body: ", req.body
+        );
       } else {
-        body.push({ type: "text", text: msg.text });
+        return Promise.all(req.body.events.map(this.processLineMessage));
       }
-    });
-  line.replyMessage(
-    messages[0].address.user.name,
-    body
-  );
+    };
+    Object.assign(line, options);
+    return this;
+  }
+
+  lineConnector.prototype.send = function(messages, cb) {
+    if (messages.length > 5)
+      return Promise.reject(
+        "BotBuilder-Line > No more than 5 messages to 1 reply token! Messages: " +
+          JSON.stringify(messages)
+      );
+    else if (messages.filter(m => m.source === "line").length !== messages.length)
+      return Promise.reject(
+        "BotBuilder-Line > Ignoring messages for other platforms..." +
+          JSON.stringify(messages)
+      );
+    if (options.debug)
+      console.log(
+        "BotBuilder-Line > Sending messages... " + JSON.stringify(messages)
+      );
+      var body = [];
+      messages.map(msg => {
+        if (msg.attachments && msg.attachments.map(t => t.contentType).filter((value, index, self) => {return self.indexOf(value) === index;}).length > 1) throw "BotBuilder-Line > All attachments in one message must have the same ContentType."
+        else if (msg.attachments && msg.attachments.length > 1 && msg.attachments.length < 11 && msg.attachmentLayout === "carousel" && msg.attachments[0].contentType === "application/vnd.microsoft.card.hero") {
+          body.push({
+            type: "template",
+            altText: getAltText(msg.text || msg.title + " " + msg.subtitle || "Please select an action."),
+            template: {
+              type: "carousel",                        
+              columns: msg.attachments.map(a => {
+                var m = {
+                  text: a.content.text,
+                  title: a.content.title || null,
+                  actions: a.content.buttons.map(b => getButtonTemp(b))
+                }
+                if (a.images) m.thumbnailImageUrl = m.content.images[0].url;
+                return m;
+              })
+            }
+          });
+        }
+        else if (msg.attachments) {
+          return msg.attachments.map(a => {
+            switch (a.contentType.split("/")[0]) {
+              case "image":
+                body.push({
+                  type: "image",
+                  originalContentUrl: a.contentUrl,
+                  previewImageUrl: a.thumbnailUrl || a.contentUrl
+                });
+                break;
+              case "video":
+                body.push({
+                  type: "video",
+                  originalContentUrl: a.contentUrl,
+                  previewImageUrl:
+                    a.thumbnailUrl ||
+                    "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/video-128.png"
+                  // Either you define a thumbnailUrl or I use a stock image
+                });
+                break;
+              case "audio":
+                body.push({
+                  type: "audio",
+                  originalContentUrl: a.contentUrl,
+                  duration: getDuration(a.contentUrl)
+                });
+                break;
+              case "application":
+                switch (a.contentType.split("/")[1]) {
+                  case "vnd.microsoft.keyboard":
+                    if (a.content.buttons.length < 5) body.push({
+                      type: "template",
+                      altText: getAltText(msg.text || msg.title + " " + msg.subtitle || "Please select an action."),
+                      template: {
+                        type: "buttons",
+                        title: msg.title || null,
+                        text: msg.text || "Please select an action.",
+                        actions: a.content.buttons.map(b => getButtonTemp(b))
+                      }
+                    })
+                    else throw "BotBuilder-Line > If you have more than 4 buttons, use a carousel of hero cards instead (Up to 10 cards with 3 buttons each)."
+                    break;
+                  case "vnd.microsoft.card.hero":
+                    if (a.content.buttons.length < 5) body.push({
+                      type: "template",
+                      altText: getAltText(a.content.text || a.content.title + " " + a.content.subtitle || "Please select an action."),
+                      template: {
+                        type: "buttons",
+                        title: a.content.title || null,
+                        text: a.content.text || "Please select an action.",
+                        actions: a.content.buttons.map(b => getButtonTemp(b))
+                      }
+                    })
+                    else throw "BotBuilder-Line > If you have more than 4 buttons, use a carousel of hero cards instead (Up to 10 cards with 3 buttons each)."
+                    break;
+                }
+                break;
+            }
+          });
+        } else {
+          body.push({ type: "text", text: msg.text });
+        }
+      });
+    line.replyMessage(
+      messages[0].address.user.name,
+      body
+    );
+  };
 };
 
 module.exports = LineConnector;
